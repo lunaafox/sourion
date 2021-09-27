4ba653ad248ba47e2bc5303d713f2b25d5a56931
lunaafox <91321411+lunaafox@users.noreply.github.com>
2021-09-27 14:51:12 +0200

    Delete Hook.h

--- Hook.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "include.h"
-
-void Patch(BYTE* dst, BYTE* src, unsigned int size);
-bool Hook(char* src, char* dst, int len);
-char* TrampHook(char* src, char* dst, unsigned int len);
